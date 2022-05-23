39948a921521cdb7afa5c8f199ec98911c41740d
sanvisawhney <96869508+sanvisawhney@users.noreply.github.com>
2022-05-23 17:48:30 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCbvU3Gyh447JV7qGBDxBppAt9Xyce7gXE",
+    authDomain: "kwitter-project-dfa2f.firebaseapp.com",
+    databaseURL: "https://kwitter-project-dfa2f-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-dfa2f",
+    storageBucket: "kwitter-project-dfa2f.appspot.com",
+    messagingSenderId: "701809369418",
+    appId: "1:701809369418:web:242fe4be3d7e71fd9921b3"
+  };
+  
+  // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
